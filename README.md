8f45829c5fcc3987766448bab792b33685501c98
Maria Sizova <sizova.maria.olegovna@yandex.ru>
2023-05-17 22:07:01 +0300

    ПР15

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
   
 Пожалуйста, прикрепите в это описание ссылку на сайт, размещенный на Яндекс.Облаке.
 
-Адрес репозитория: https://github.com/...
+Адрес репозитория: https://github.com/Argayash1/react-mesto-api-full-gha
 
 ## Ссылки на проект
 
-IP-адрес x.x.x.x
+IP 130.193.40.103
 
-Frontend https://...
+Frontend https://mesto2023.students.nomoredomains.monster
 
-Backend https://...
+Backend https://apii.mesto2023.students.nomoredomains.monster
---
+++ backend/app.js
@@ -0,0 +1,56 @@
+// Импорт модуля dotenv для добавления переменных окружения в process.env
+require('dotenv').config();
+
+// Импорт npm-пакетов
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Импорт миддлвэров
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const errorHandler = require('./middlewares/errorHandler');
+const limiter = require('./middlewares/limiter');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsHandler = require('./middlewares/corsHandler');
+
+// Импорт роутера
+const router = require('./routes/index');
+
+// Импорт переменных
+const { PORT, DB } = require('./utils/config');
+
+// Создаём приложение на express
+const app = express(); // Cоздаём приложение методом express
+
+// подключаемся к серверу mongo
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+});
+
+app.use(corsHandler);
+
+// Миддлвэры для безопасности (лимитер и хельмет)
+app.use(limiter);
+app.use(helmet());
+
+// Миддлвэры для парсинга
+app.use(express.json()); // для собирания JSON-формата
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // подключаем парсер кук как мидлвэр
+
+// Миддлвэр-логгер запросов
+app.use(requestLogger); // подключаем логгер запросов
+
+// Роутер
+app.use(router);
+
+// Миддлвэры для обработки ошибок
+app.use(errorLogger); // подключаем логгер ошибок
+app.use(errors()); // обработчик ошибок celebrate
+app.use(errorHandler); // централизолванная обработка ошибок
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
